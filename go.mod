4a4ea04a90ab4223f8dd4d7833e7f0c906c7e03f
Lori Karikari <lori.karikari@gmail.com>
2026-05-03 21:52:18 +0200

    feat(docker): add SDK wrapper with network and Swarm service ops (#19)

--- go.mod
+++ go.mod
@@ -5,20 +5,41 @@ go 1.25.2
 require (
 	connectrpc.com/connect v1.19.2
 	filippo.io/age v1.3.1
+	github.com/google/uuid v1.6.0
+	github.com/moby/moby/api v1.54.2
+	github.com/moby/moby/client v0.4.1
 	google.golang.org/protobuf v1.36.11
 	modernc.org/sqlite v1.50.0
 )
 
 require (
 	filippo.io/hpke v0.4.0 // indirect
+	github.com/Microsoft/go-winio v0.6.2 // indirect
+	github.com/containerd/errdefs v1.0.0 // indirect
+	github.com/containerd/errdefs/pkg v0.3.0 // indirect
+	github.com/distribution/reference v0.6.0 // indirect
+	github.com/docker/go-connections v0.7.0 // indirect
+	github.com/docker/go-units v0.5.0 // indirect
 	github.com/dustin/go-humanize v1.0.1 // indirect
-	github.com/google/uuid v1.6.0 // indirect
+	github.com/felixge/httpsnoop v1.0.4 // indirect
+	github.com/go-logr/logr v1.4.2 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/moby/docker-image-spec v1.3.1 // indirect
 	github.com/ncruces/go-strftime v1.0.0 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/opencontainers/image-spec v1.1.1 // indirect
 	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
+	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.60.0 // indirect
+	go.opentelemetry.io/otel v1.35.0 // indirect
+	go.opentelemetry.io/otel/metric v1.35.0 // indirect
+	go.opentelemetry.io/otel/trace v1.35.0 // indirect
 	golang.org/x/crypto v0.45.0 // indirect
 	golang.org/x/sys v0.42.0 // indirect
 	modernc.org/libc v1.72.0 // indirect
 	modernc.org/mathutil v1.7.1 // indirect
 	modernc.org/memory v1.11.0 // indirect
 )
+
+replace github.com/docker/docker/api/types/network => github.com/moby/moby/api/types/network v1.54.2
---
+++ internal/docker/client.go
@@ -0,0 +1,139 @@
+package docker
+
+import (
+	"context"
+	"fmt"
+	"net/netip"
+
+	"github.com/moby/moby/api/types/network"
+	"github.com/moby/moby/api/types/swarm"
+	"github.com/moby/moby/client"
+)
+
+type Client struct {
+	inner *client.Client
+}
+
+type NetworkCreateSpec struct {
+	Name    string
+	Driver  string
+	Subnet  netip.Prefix
+	Gateway netip.Addr
+}
+
+type Network struct {
+	ID   string
+	Name string
+}
+
+type ServiceCreateSpec struct {
+	Name    string
+	Image   string
+	Env     []string
+	Network string
+}
+
+type Service struct {
+	ID   string
+	Name string
+}
+
+func New() (*Client, error) {
+	cli, err := client.New(client.FromEnv)
+	if err != nil {
+		return nil, fmt.Errorf("docker client: %w", err)
+	}
+	return &Client{inner: cli}, nil
+}
+
+func (c *Client) NetworkCreate(ctx context.Context, spec NetworkCreateSpec) (*Network, error) {
+	resp, err := c.inner.NetworkCreate(ctx, spec.Name, client.NetworkCreateOptions{
+		Driver: spec.Driver,
+		IPAM: &network.IPAM{
+			Config: []network.IPAMConfig{
+				{Subnet: spec.Subnet, Gateway: spec.Gateway},
+			},
+		},
+	})
+	if err != nil {
+		return nil, fmt.Errorf("network create: %w", err)
+	}
+	return &Network{ID: resp.ID, Name: spec.Name}, nil
+}
+
+func (c *Client) NetworkInspect(ctx context.Context, id string) (*Network, error) {
+	resp, err := c.inner.NetworkInspect(ctx, id, client.NetworkInspectOptions{})
+	if err != nil {
+		return nil, fmt.Errorf("network inspect: %w", err)
+	}
+	return &Network{ID: resp.Network.ID, Name: resp.Network.Name}, nil
+}
+
+func (c *Client) NetworkExists(ctx context.Context, name string) (bool, error) {
+	nets, err := c.inner.NetworkList(ctx, client.NetworkListOptions{
+		Filters: client.Filters{"name": {name: true}},
+	})
+	if err != nil {
+		return false, fmt.Errorf("network list: %w", err)
+	}
+	for _, net := range nets.Items {
+		if net.Name == name {
+			return true, nil
+		}
+	}
+	return false, nil
+}
+
+func (c *Client) NetworkRemove(ctx context.Context, id string) error {
+	_, err := c.inner.NetworkRemove(ctx, id, client.NetworkRemoveOptions{})
+	if err != nil {
+		return fmt.Errorf("network remove: %w", err)
+	}
+	return nil
+}
+
+func (c *Client) ServiceCreate(ctx context.Context, spec ServiceCreateSpec) (*Service, error) {
+	task := swarm.TaskSpec{
+		ContainerSpec: &swarm.ContainerSpec{
+			Image: spec.Image,
+			Env:   spec.Env,
+		},
+	}
+	if spec.Network != "" {
+		task.Networks = []swarm.NetworkAttachmentConfig{{Target: spec.Network}}
+	}
+
+	resp, err := c.inner.ServiceCreate(ctx, client.ServiceCreateOptions{
+		Spec: swarm.ServiceSpec{
+			Annotations:  swarm.Annotations{Name: spec.Name},
+			TaskTemplate: task,
+		},
+	})
+	if err != nil {
+		return nil, fmt.Errorf("service create: %w", err)
+	}
+	return &Service{ID: resp.ID, Name: spec.Name}, nil
+}
+
+func (c *Client) ServiceExists(ctx context.Context, name string) (bool, error) {
+	svcs, err := c.inner.ServiceList(ctx, client.ServiceListOptions{
+		Filters: client.Filters{"name": {name: true}},
+	})
+	if err != nil {
+		return false, fmt.Errorf("service list: %w", err)
+	}
+	for _, svc := range svcs.Items {
+		if svc.Spec.Name == name {
+			return true, nil
+		}
+	}
+	return false, nil
+}
+
+func (c *Client) ServiceRemove(ctx context.Context, id string) error {
+	_, err := c.inner.ServiceRemove(ctx, id, client.ServiceRemoveOptions{})
+	if err != nil {
+		return fmt.Errorf("service remove: %w", err)
+	}
+	return nil
+}
